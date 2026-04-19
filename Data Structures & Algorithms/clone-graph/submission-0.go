b7724482db39cd1a366e50bac5bcc02dbbef0ea5
falearn <127325061+falearn228@users.noreply.github.com>
2026-04-19 16:23:21 +0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.go
@@ -0,0 +1,34 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+
+func cloneGraph(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+
+	vtd := make(map[*Node]*Node)
+    var recur func(curr *Node) *Node
+	recur = func(curr *Node) *Node {
+		if clone, ok := vtd[curr]; ok {
+			return clone
+		}
+
+		clone := &Node{
+			Val: curr.Val,
+			Neighbors: make([]*Node, len(curr.Neighbors)),
+		}
+
+		vtd[curr] = clone
+		for i, nei := range curr.Neighbors {
+			clone.Neighbors[i] = recur(nei)
+		}
+		return clone
+	}
+	chmo := recur(node)  
+	return chmo
+}
