b9305d22b3ec65d48353e99e35ccb545b59768de
falearn <127325061+falearn228@users.noreply.github.com>
2026-04-19 14:44:08 +0700

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.go
@@ -0,0 +1,33 @@
+func maxAreaOfIsland(grid [][]int) int {
+    n := len(grid)
+    m := len(grid[0])
+    var ans, maxAns int
+    var dfs func(r, c int) int
+    dfs = func(r, c int) int {
+        if !check(r, c, n, m, grid) {
+            return 0
+        }
+
+        grid[r][c] = 2
+        var zal = dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
+        return 1 + zal
+    }
+
+    for r := range n {
+        for c := range m {
+            if grid[r][c] == 1 {
+                ans = dfs(r, c)
+            }
+            maxAns = max(ans, maxAns)
+        }
+    }
+
+    return maxAns
+}
+
+func check(r, c, n, m int, grid [][]int) bool {
+    if r < 0 || c < 0 || r >= n || c >= m || grid[r][c] != 1 {
+        return false
+    }
+    return true
+}
