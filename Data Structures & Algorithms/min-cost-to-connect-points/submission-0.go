905c9151f496d1a941f10d8f38dde7594caf43a8
falearn <127325061+falearn228@users.noreply.github.com>
2026-04-21 13:29:25 +0700

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.go
@@ -0,0 +1,32 @@
+func minCostConnectPoints(points [][]int) int {
+    n := len(points)
+    node := 0
+    dist := make([]int, n)
+    for i := range dist {
+        dist[i] = math.MaxInt
+    }
+    visit := make([]bool, n)
+    edges, res := 0, 0
+
+    for edges < n-1 {
+        visit[node] = true
+        nextNode := -1
+        for i := 0; i < n; i++ {
+            if visit[i] {
+                continue
+            }
+            curDist := int(math.Abs(float64(points[i][0]-points[node][0])) +
+				math.Abs(float64(points[i][1]-points[node][1])))
+            if curDist < dist[i] {
+            dist[i] = curDist
+            }
+            if nextNode == -1 || dist[i] < dist[nextNode] {
+                nextNode = i
+            }
+        }
+        res += dist[nextNode]
+        node = nextNode
+        edges++
+    }
+    return res
+}
