d7ef21db4ddaedafb0c74559c0b1a300f8520b02
falearn <127325061+falearn228@users.noreply.github.com>
2026-04-29 17:08:04 +0700

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.go
@@ -0,0 +1,12 @@
+func rob(nums []int) int {
+	n := len(nums)
+    dp := make([]int, n+1)
+
+	dp[1] = nums[0]
+
+	for i := 2; i <= n; i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i-1])
+	}
+
+	return dp[n]
+}
